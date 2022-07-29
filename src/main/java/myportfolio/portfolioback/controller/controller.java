db12c7bac99f461fae9a0d7b26c0163df9ee325e
Leonardo J. Olmedo <leoolmedo@hotmail.com>
2022-07-29 12:40:19 -0300

    @ReverseOrigins updated to include firebase address

--- src/main/java/myportfolio/portfolioback/controller/controller.java
+++ src/main/java/myportfolio/portfolioback/controller/controller.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200", "https://evening-stream-47799.herokuapp.com:80"})
 @RestController
 public class controller {
    
